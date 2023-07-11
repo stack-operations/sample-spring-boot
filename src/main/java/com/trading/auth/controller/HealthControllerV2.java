7c13800d87983ff6723f798bbc8d46c109cdaf40
sumit <sumit@compensationcloud.com>
2023-07-11 12:48:21 +0530

    controller changes

--- src/main/java/com/trading/auth/controller/HealthControllerV2.java
+++ src/main/java/com/trading/auth/controller/HealthControllerV2.java
@@ -4,22 +4,14 @@ package com.trading.auth.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/v1/user-svc")
-public class HealthController {
+@RequestMapping("/v2/user-svc")
+public class HealthControllerV2 {
 
     @Autowired
     ApplicationContext applicationContext;
@@ -30,14 +22,14 @@ public class HealthController {
 	@RequestMapping("/health")
     public String getSampleMessage()
     {
-        return environment.getProperty("application.name","App")+" V1 is healthy";
+        return environment.getProperty("application.name","App")+" V2 is healthy";
     }
 
     @RequestMapping("/ping")
     public String ping()
     {
         System.out.println("Current time is " + new Date().toString());
-        return "App is healthy";
+        return "V2 App is healthy";
     }
 
 }
